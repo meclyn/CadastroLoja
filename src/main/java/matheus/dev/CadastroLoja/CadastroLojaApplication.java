f780d06e3677721e3065c2d9e59fa09670e81172
Matheus <matheusferreirasjc@gmail.com>
2025-09-22 19:13:48 -0300

    Primeiro commit - CadastroLoja

---
+++ src/main/java/matheus/dev/CadastroLoja/CadastroLojaApplication.java
@@ -0,0 +1,13 @@
+package matheus.dev.CadastroLoja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matheus/dev/CadastroLoja/CadastroLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package matheus.dev.CadastroLoja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
