fd08418a8e27d6b67d7f8c5f4c5a716cb5f5ef0e
Matheus <matheusferreirasjc@gmail.com>
2025-09-22 19:26:54 -0300

    feat: Cria o Controller

---
+++ src/main/java/matheus/dev/CadastroLoja/LojaController.java
@@ -0,0 +1,16 @@
+package matheus.dev.CadastroLoja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class LojaController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Boas Vindas";
+
+    }
+}
