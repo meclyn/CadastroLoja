5a985508b30dbf2ceb233ba629a80b4ef1c8d753
Matheus <matheusferreirasjc@gmail.com>
2025-09-25 21:54:00 -0300

    build: Conecta o banco de dados h2 a minha aplicacao SPRING

--- src/main/java/matheus/dev/CadastroLoja/Produto/ProdutoController.java
+++ src/main/java/matheus/dev/CadastroLoja/Produto/ProdutoController.java
@@ -8,9 +8,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class ProdutoController {
 
-    @GetMapping("/boasvindas")
-    public String boasVindas(){
-        return "Boas Vindas";
-
     }
-}
