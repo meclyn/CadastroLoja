a168fb3318b31527284de6e3d2f32e7c9492f8c9
Matheus <matheusferreirasjc@gmail.com>
2025-09-30 20:39:47 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/matheus/dev/CadastroLoja/Cliente/ClienteController.java
+++ src/main/java/matheus/dev/CadastroLoja/Cliente/ClienteController.java
@@ -1,9 +1,5 @@
 package matheus.dev.CadastroLoja.Cliente;
-
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +8,35 @@ public class ClienteController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "boas vindas ao /cliente";
+    }
+
+    //Adicionar Cliente
+    @PostMapping("/criarCliente")
+    public String criarCliente(){
+        return "Cliente criado com sucesso!";
+    }
+    
+    //Procurar Cliente por ID
+    @GetMapping("/listarID")
+    public String listarID(){
+        return "Cliente encontrado com sucesso!";
+    }
+
+    //Mostrar todos os Clientes
+    @GetMapping("/listarTodos")
+    public String listarTodos() {
+        return "Lista de Clientes!";
+    }
+
+    //Alterar dados do Cliente
+    @PutMapping("/alterarCliente")
+    public String alterarCliente(){
+        return "Cliente alterado com sucesso!";
+    }
+
+    //Deletar Cliente
+    @PostMapping("/deletarCliente")
+    public String deletarCliente(){
+        return "Cliente deletado com sucesso!";
     }
 }
--- src/main/java/matheus/dev/CadastroLoja/Produto/ProdutoController.java
+++ src/main/java/matheus/dev/CadastroLoja/Produto/ProdutoController.java
@@ -1,11 +1,38 @@
 package matheus.dev.CadastroLoja.Produto;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class ProdutoController {
 
+    //Adicionar Produto
+    @PostMapping("/criarProduto")
+    public String criarProduto(){
+        return "Produto criado com sucesso!";
     }
+
+    //Procurar Produto por ID
+    @GetMapping("/listarPID")
+    public String listarProdutoID(){
+        return "Produto listado com sucesso!";
+    }
+
+    //Listar todos os Produtos
+    @GetMapping("/listarTodosProdutos")
+        public String listarProdutos(){
+        return "Produtos listados com sucesso!";
+    }
+
+    //Alterar dados do Produto
+    @PutMapping("/alterarProduto")
+    public String alterarProduto() {
+        return "Produto alterado com sucesso!";
+    }
+
+    //Deletar Produto
+    @PostMapping("/deletarProduto")
+    public String deletarProduto() {
+        return "Produto deletado com sucesso!";
+    }
+}
