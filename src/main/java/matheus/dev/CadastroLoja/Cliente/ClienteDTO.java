f5e599dc68711a9869ae75a2725ab3c48b388189
Matheus <matheusferreirasjc@gmail.com>
2025-10-02 22:27:43 -0300

    chore: Inclui a versao do DTO pela entidade e mapeia manualmente no Mapper os caminhos Entidade x DTO

--- src/main/java/matheus/dev/CadastroLoja/Cliente/ClienteDTO.java
+++ src/main/java/matheus/dev/CadastroLoja/Cliente/ClienteDTO.java
@@ -1,4 +1,22 @@
 package matheus.dev.CadastroLoja.Cliente;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import matheus.dev.CadastroLoja.Produto.ProdutoModel;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClienteDTO {
+
+    private Long id;
+    private String nome;
+    private int idade;
+    private String email;
+    private String cpf;
+    private String sexo;
+    private String telefone;
+    private String endereco;
+    private ProdutoModel produtos;
 }
