385698ff569c272055cfaac97bb32ff4bc857a7f
Matheus <matheusferreirasjc@gmail.com>
2025-10-06 20:42:42 -0300

    refactor: Refatora o controller para incluir o ResponseEntity nas rotas e customizar a resposta das requisicoes do cliente

--- src/main/java/matheus/dev/CadastroLoja/Cliente/ClienteController.java
+++ src/main/java/matheus/dev/CadastroLoja/Cliente/ClienteController.java
@@ -1,4 +1,8 @@
 package matheus.dev.CadastroLoja.Cliente;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,33 +24,59 @@ public class ClienteController {
 
     //Adicionar Cliente
     @PostMapping("/criar")
-    public ClienteDTO criarCliente(@RequestBody ClienteDTO cliente){
-        return clienteService.criarCliente(cliente);
+    public ResponseEntity<String> criarCliente(@RequestBody ClienteDTO cliente){
+
+        ClienteDTO clienteDTO = clienteService.criarCliente(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Cliente criado com sucesso: " + clienteDTO.getNome() + " (ID): " + clienteDTO.getId());
     }
     
     //Procurar Cliente por ID
     @GetMapping("/listar/{id}")
-    public ClienteDTO listarClientePorId(@PathVariable Long id){
-        return clienteService.listarClientesPorId(id);
+    public ResponseEntity<?> listarClientePorId(@PathVariable Long id){
+
+        ClienteDTO clientes = clienteService.listarClientesPorId(id);
+        if(clientes != null) {
+            return ResponseEntity.ok(clientes);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente com o ID " + id + " não encontrado");
+        }
     }
 
     //Mostrar todos os Clientes
     @GetMapping("/listar")
-    public List<ClienteDTO> listarClientes() {
-        return clienteService.listarClientes();
+    public ResponseEntity<List<ClienteDTO>> listarClientes() {
+        List<ClienteDTO> clientes = clienteService.listarClientes();
+        return ResponseEntity.ok(clientes);
     }
 
     //Alterar dados do Cliente
     @PutMapping("/alterar/{id}")
-    public ClienteDTO alterarCliente(@PathVariable Long id, @RequestBody ClienteDTO clienteAtualizado){
-        return clienteService.atualizarCliente(id, clienteAtualizado);
+    public ResponseEntity<?> alterarCliente(@PathVariable Long id, @RequestBody ClienteDTO clienteAtualizado){
+
+        ClienteDTO cliente = clienteService.atualizarCliente(id, clienteAtualizado);
+
+        if (cliente != null) {
+            return ResponseEntity
+                    .ok(cliente);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O cliente com o ID " + id + " não foi encontrado");
+        }
     }
 
     //Deletar Cliente
     @PostMapping("/deletar/{id}")
-    public void deletarCliente(@PathVariable Long id){
-        clienteService.deletarCliente(id);
+    public ResponseEntity<String> deletarCliente(@PathVariable Long id) {
+
+        if (clienteService.listarClientesPorId(id) != null) {
+            clienteService.deletarCliente(id);
+            return ResponseEntity.ok("Cliente com o ID " + id + " deletado com sucesso");
+
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O cliente com o ID " + id + " não foi encontrado");
+        }
 
     }
-
 }
--- src/main/java/matheus/dev/CadastroLoja/Produto/ProdutoController.java
+++ src/main/java/matheus/dev/CadastroLoja/Produto/ProdutoController.java
@@ -1,5 +1,8 @@
 package matheus.dev.CadastroLoja.Produto;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,31 +19,59 @@ public class ProdutoController {
 
     //Adicionar Produto
     @PostMapping("/criar")
-    public ProdutoDTO criarProduto(@RequestBody ProdutoDTO produto){
-        return produtoService.criarProduto(produto);
+    public ResponseEntity<String> criarProduto(@RequestBody ProdutoDTO produto){
+         ProdutoDTO produtoDTO = produtoService.criarProduto(produto);
+         return ResponseEntity.status(HttpStatus.CREATED)
+                 .body("Produto criado com sucesso" + produtoDTO.getNome() + "ID: " + produtoDTO.getId());
     }
 
     //Procurar Produto por ID
     @GetMapping("/listarID")
-    public ProdutoDTO listarProdutoPorId(@PathVariable Long id){
-        return produtoService.listarProdutoPorId(id);
+    public ResponseEntity<?> listarProdutoPorId(@PathVariable Long id){
+
+        ProdutoDTO produto = produtoService.listarProdutoPorId(id);
+
+        if(produto != null) {
+            return ResponseEntity.ok(produto);
+
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Produto com o ID " + id + " não foi encontrado");
+        }
     }
 
     //Listar todos os Produtos
     @GetMapping("/listar")
-        public List<ProdutoDTO> listarProdutos(){
-        return produtoService.listarProdutos();
+    public ResponseEntity<List<ProdutoDTO>> listarProdutos(){
+        List<ProdutoDTO> produto = produtoService.listarProdutos();
+        return ResponseEntity.ok(produto);
     }
 
     //Alterar dados do Produto
     @PutMapping("/alterar/{id}")
-    public ProdutoDTO atualizarProduto(@PathVariable Long id, @RequestBody ProdutoDTO produtoAtualizado) {
-        return produtoService.atualizarProduto(id, produtoAtualizado);
+    public ResponseEntity<?> atualizarProduto(@PathVariable Long id, @RequestBody ProdutoDTO produtoAtualizado) {
+
+        ProdutoDTO produto = produtoService.atualizarProduto(id, produtoAtualizado);
+
+        if (produto != null) {
+            return ResponseEntity
+                    .ok(produto);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Produto com o ID " + id + " não foi encontrado");
+        }
     }
 
     //Deletar Produto
     @PostMapping("/deletar/{id}")
-    public void deletarProduto(@PathVariable Long id) {
-        produtoService.deletarProduto(id);
+    public ResponseEntity<String> deletarProduto(@PathVariable Long id) {
+        if(produtoService.listarProdutoPorId(id) != null){
+            produtoService.deletarProduto(id);
+            return ResponseEntity.ok("Produto com o ID " + id + " deletado com sucesso");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Produto com o ID " + id + " não encontrado");
+        }
+
     }
 }
